329b1335e3ba412428a3bd0638f1d0a8e594119f
estelle-cs <76448914+estelle-cs@users.noreply.github.com>
2023-02-10 14:12:47 +0100

    new : header + navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,38 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
-import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbCardModule } from "@nebular/theme";
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbCardModule,
+  NbSelectModule,
+  NbInputModule,
+  NbDatepickerModule,
+  NbButtonModule,
+} from "@nebular/theme";
 import { AppRoutingModule } from "../app/app-routing.module";
 
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { AddSextoyComponent } from "./add-sextoy/add-sextoy.component";
+import { ReactiveFormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, AddSextoyComponent],
   imports: [
     AppRoutingModule,
+    ReactiveFormsModule,
     BrowserModule,
     NbThemeModule.forRoot(),
     NbLayoutModule,
     NbSidebarModule.forRoot(),
     NbCardModule,
+    NbSelectModule,
+    NbInputModule,
+    NbDatepickerModule.forRoot(),
+    NbButtonModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
