598a1b450786ec39ca5a64f9f46b58e8a86d53e2
estelle-cs <76448914+estelle-cs@users.noreply.github.com>
2023-02-10 18:33:20 +0100

    improvement : home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
+import { map } from "rxjs";
+import { ProductService } from "../services/products.service";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.scss"],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  public products$: any;
 
+  constructor(private productService: ProductService, private router: Router) {}
+
+  public ngOnInit() {
+    this.products$ = this.productService.getAllProducts();
+  }
+
+  public getCatalogueName(catalogId: number) {
+    return this.productService.getCatalogWithId(catalogId);
+  }
+
+  public redirect(productId: number) {
+    this.router.navigate(["/product"], { queryParams: { id: productId } });
+  }
 }
