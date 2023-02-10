43dc727ab8b9dea4ec35037c9d3df2672e0b2abf
estelle-cs <76448914+estelle-cs@users.noreply.github.com>
2023-02-10 18:33:54 +0100

    improvement : product service + tsconfig

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -8,32 +8,40 @@ export class ProductService {
   constructor(private httpclient: HttpClient) {}
 
   public getAllProducts() {
-    return this.httpclient.get("leod1.site:8877" + `/product/getAllProduct`);
+    return this.httpclient.get(
+      "http://leod1.site:8877" + `/product/getAllProduct`
+    );
   }
 
   public getOneProduct(productId: any) {
     return this.httpclient.get(
-      "leod1.site:8877" + `/product/getProduct/${productId}`
+      "http://leod1.site:8877" + `/product/getProduct/${productId}`
     );
   }
 
   public addProduct(data: any) {
     return this.httpclient.post(
-      "leod1.site:8877" + `/product/createProduct`,
+      "http://leod1.site:8877" + `/product/createProduct`,
       data
     );
   }
 
   public updateProduct(updatedData: any) {
     return this.httpclient.put(
-      "leod1.site:8877" + `/product/updateProduct/${updatedData.id}`,
+      "http://leod1.site:8877" + `/product/updateProduct/${updatedData.id}`,
       updatedData
     );
   }
 
   public deleteProduct(productId: any) {
     return this.httpclient.delete(
-      "leod1.site:8877" + `/product/deleteProduct/${productId}`
+      "http://leod1.site:8877" + `/product/deleteProduct/${productId}`
+    );
+  }
+
+  public getCatalogWithId(catalogId: number) {
+    return this.httpclient.get(
+      "http://leod1.site:8877" + `/catalogue/getCatalogue/${catalogId}`
     );
   }
 }
