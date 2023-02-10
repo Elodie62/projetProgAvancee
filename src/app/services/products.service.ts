9e53a3a28cb69bca3822f6b085619fea634624bd
estelle-cs <76448914+estelle-cs@users.noreply.github.com>
2023-02-10 14:39:12 +0100

    new service to develop

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -5,34 +5,34 @@ import { Injectable } from "@angular/core";
   providedIn: "root",
 })
 export class ProductService {
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpclient: HttpClient) {}
 
   public getAllProducts() {
-    return this.httpClient.get("leod1.site:8877" + `/product/getAllProduct`);
+    return this.httpclient.get("leod1.site:8877" + `/product/getAllProduct`);
   }
 
   public getOneProduct(productId: any) {
-    return this.httpClient.get(
+    return this.httpclient.get(
       "leod1.site:8877" + `/product/getProduct/${productId}`
     );
   }
 
   public addProduct(data: any) {
-    return this.httpClient.post(
+    return this.httpclient.post(
       "leod1.site:8877" + `/product/createProduct`,
       data
     );
   }
 
   public updateProduct(updatedData: any) {
-    return this.httpClient.put(
+    return this.httpclient.put(
       "leod1.site:8877" + `/product/updateProduct/${updatedData.id}`,
       updatedData
     );
   }
 
   public deleteProduct(productId: any) {
-    return this.httpClient.delete(
+    return this.httpclient.delete(
       "leod1.site:8877" + `/product/deleteProduct/${productId}`
     );
   }
