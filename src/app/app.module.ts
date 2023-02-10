5467b3cd0bbc734eb91c9a96024faaba9ce6a41a
Elodie <eleclercq@myges.fr>
2023-02-10 14:31:08 +0100

    resolve conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,38 @@
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
-<<<<<<< Updated upstream
-import { NbThemeModule, NbLayoutModule, NbSidebarModule, NbCardModule } from "@nebular/theme";
-import { AppRoutingModule } from "../app/app-routing.module";
+import { NbEvaIconsModule } from "@nebular/eva-icons";
+import {
+  NbThemeModule,
+  NbLayoutModule,
+  NbSidebarModule,
+  NbCardModule,
+  NbButtonModule,
+  NbInputModule,
+  NbIconModule,
+} from "@nebular/theme";
 
+import { AppRoutingModule } from "../app/app-routing.module";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
+import { ProductComponent } from "./product/product.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, ProductComponent],
   imports: [
     AppRoutingModule,
     BrowserModule,
+    HttpClientModule,
     NbThemeModule.forRoot(),
     NbLayoutModule,
     NbSidebarModule.forRoot(),
     NbCardModule,
+    NbButtonModule,
+    NbInputModule,
+    NbIconModule,
+    NbEvaIconsModule,
+    FormsModule,
   ],
-=======
-import { HttpClientModule } from "@angular/common/http";
-import { AppComponent } from "./app.component";
-
-@NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
->>>>>>> Stashed changes
-  providers: [],
-  bootstrap: [AppComponent],
 })
 export class AppModule {}
