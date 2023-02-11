0c09fe4f5825167e0ef46688bdd40f208ede2b66
Elodie <eleclercq@myges.fr>
2023-02-11 22:55:30 +0100

    productPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AddSextoyComponent } from "./add-sextoy/add-sextoy.component";
 import { HomeComponent } from "./home/home.component";
+import { ProductComponent } from "./product/product.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "list", component: HomeComponent },
   { path: "addSextoy", component: AddSextoyComponent },
+  { path: "product", component: ProductComponent },
 ];
 
 @NgModule({
