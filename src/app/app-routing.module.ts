9ca225eed7719162c9119aefc956f62c201fe9db
estelle-cs <76448914+estelle-cs@users.noreply.github.com>
2023-02-11 17:15:39 +0100

    new addsextoy component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
-//import { AddSextoyComponent } from "./add-sextoy/add-sextoy.component";
+import { AddSextoyComponent } from "./add-sextoy/add-sextoy.component";
 import { HomeComponent } from "./home/home.component";
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
   { path: "list", component: HomeComponent },
-  //{ path: "addSextoy", component: AddSextoyComponent },
+  { path: "addSextoy", component: AddSextoyComponent },
 ];
 
 @NgModule({
